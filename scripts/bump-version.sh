10e77fff4ef62b70800b4eedda90ed6008d1e566
Hosh Sadiq <600045+hoshsadiq@users.noreply.github.com>
2026-03-07 12:15:21 +0400

    chore: script formatting

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 version="$1"
 
-patch "charts/linkerd-trust-anchor-refresh/Chart.yaml" <<< $(
+patch "charts/linkerd-trust-anchor-refresh/Chart.yaml" <<<"$(
   command diff -U0 -w -b --ignore-blank-lines \
     <(yq '.' charts/linkerd-trust-anchor-refresh/Chart.yaml) \
     <(
@@ -13,10 +13,10 @@ patch "charts/linkerd-trust-anchor-refresh/Chart.yaml" <<< $(
         .appVersion =  strenv(version)
       " charts/linkerd-trust-anchor-refresh/Chart.yaml
     )
-  )
+)"
 
-patch "charts/linkerd-trust-anchor-refresh/values.yaml" <<< $(
+patch "charts/linkerd-trust-anchor-refresh/values.yaml" <<<"$(
   command diff -U0 -w -b --ignore-blank-lines \
     <(yq '.' charts/linkerd-trust-anchor-refresh/values.yaml) \
     <(version="$version" yq eval ".image.tag = strenv(version)" charts/linkerd-trust-anchor-refresh/values.yaml)
-  )
+)"
